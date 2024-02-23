1c7302ac36ce95bc6391d2e8f79aa9cb378f03e0
mahdi hosseini <hosseinim018@gmail.com>
2024-02-23 19:30:29 +0330

    Implemented Customers model with required field 'customer_name', address, phone, status, and comments.

--- myapp/models.py
+++ myapp/models.py
@@ -6,6 +6,7 @@ from django.db import models
 from django.db import models
 import uuid
 
+
 class Shipment(models.Model):
     """
     Model representing a shipment with various attributes related to its status, location, and associated entities.
@@ -147,6 +148,7 @@ class Truck(models.Model):
     def __str__(self):
         return self.license_number
 
+
 class Supplier(models.Model):
     supplier_id = models.AutoField(primary_key=True)
     supplier_name = models.CharField(max_length=255, null=False)
@@ -157,6 +159,7 @@ class Supplier(models.Model):
 
     def __str__(self):
         return self.supplier_name
+
 
 class Product(models.Model):
     reel_number = models.CharField(max_length=255, primary_key=True)
@@ -177,3 +180,18 @@ class Product(models.Model):
 
     def __str__(self):
         return f"Product (Reel Number: {self.reel_number}, Status: {self.status})"
+
+
+from django.db import models
+
+
+class Customer(models.Model):
+    customer_id = models.AutoField(primary_key=True)
+    customer_name = models.CharField(max_length=255, null=False)
+    address = models.TextField(blank=True)
+    phone = models.CharField(max_length=20, blank=True)
+    status = models.CharField(max_length=255, blank=True)
+    comments = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.customer_name
