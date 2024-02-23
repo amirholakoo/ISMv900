0fe3e6c2dd698b153d4d8e5441d50d8f43315353
mahdi hosseini <hosseinim018@gmail.com>
2024-02-23 19:47:23 +0330

    Implement Anbar_Akhal model with foreign key to Supplier, additional fields for product details, and status choices.

--- myapp/models.py
+++ myapp/models.py
@@ -378,9 +378,6 @@ class Anbar_Khamir_Kordan(models.Model):
         return f"Anbar_Khamir_Kordan (ID: {self.id}, Reel Number: {self.reel_number})"
 
 
-from django.db import models
-from datetime import datetime
-
 class AnbarMuhvatehKardan(models.Model):
     id = models.AutoField(primary_key=True)
     receive_date = models.DateTimeField(null=True)
@@ -405,3 +402,27 @@ class AnbarMuhvatehKardan(models.Model):
 
     def __str__(self):
         return f"AnbarMuhvatehKardan (Reel Number: {self.reel_number}, Status: {self.status})"
+
+
+class Anbar_Akhal(models.Model):
+    id = models.AutoField(primary_key=True)
+    receive_date = models.DateTimeField()
+    reel_number = models.CharField(max_length=255)
+    supplier = models.ForeignKey(Supplier, on_delete=models.CASCADE)
+    supplier_name = models.CharField(max_length=255)
+    material_type = models.CharField(max_length=255)
+    material_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True)
+    status = models.CharField(max_length=10, choices=[('In-stock', 'In-stock'), ('Moved', 'Moved'), ('Used', 'Used')])
+    location = models.CharField(max_length=255, blank=True)
+    last_date = models.DateTimeField(blank=True, null=True)
+    width = models.IntegerField(blank=True, null=True)
+    gsm = models.IntegerField(blank=True, null=True)
+    length = models.IntegerField(blank=True, null=True)
+    grade = models.CharField(max_length=255, blank=True, null=True)
+    breaks = models.CharField(max_length=255, blank=True, null=True)
+    comments = models.TextField(blank=True)
+    qr_code = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Anbar Akhal (Reel Number: {self.reel_number}, Status: {self.status})"
