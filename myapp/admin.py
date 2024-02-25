ead7adeb1648a99221b3073649d1b66852574bc4
mahdi hosseini <hosseinim018@gmail.com>
2024-02-25 09:29:52 +0330

    created TruckAdmin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+@admin.register(Truck)
+class TruckAdmin(admin.ModelAdmin):
+    list_display = ('license_number', 'driver_name', 'phone', 'status', 'location')
+    search_fields = ('license_number', 'driver_name')
+    list_filter = ('status', 'location')  # Add filters
+    ordering = ['-license_number']  # Sort by license number in descending order
+
