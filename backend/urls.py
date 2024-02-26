586de18e65cb5a8f3f5b9e7ead137005b05d0b37
mahdi hosseini <hosseinim018@gmail.com>
2024-02-26 10:02:33 +0330

    fix issue of inclue

--- backend/urls.py
+++ backend/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("myapp/", include('myapp/urls'), name='myapp'),
+    path("myapp/", include('myapp.urls'), name='myapp'),
 ]
