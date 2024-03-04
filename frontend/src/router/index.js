ab82a5267aeff617b57f336cad7711fbe4b21002
mahdi hosseini <hosseinim018@gmail.com>
2024-03-04 12:36:14 +0330

    added new route addTrucl and config router

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import addTruck from "@/components/addTruck.vue";
 
 const routes = [
   {
@@ -14,12 +15,18 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  },
+  {
+    path: '/myapp/addTruck/',
+    name: 'addTruck',
+    component: addTruck
+  },
 ]
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes
+  // history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
