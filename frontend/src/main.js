98d50eeda4bb03fc18ccccb99113dbf2cf8b4d41
mahdi hosseini <hosseinim018@gmail.com>
2024-03-04 12:35:21 +0330

    config and use axios in app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import './assets/tailwind.css'
 
-createApp(App).use(router).mount('#app')
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+
+const app = createApp(App)
+
+app.use(VueAxios, axios)
+app.use(router)
+app.mount('#app')
