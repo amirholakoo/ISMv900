1083c91372bc17d0cf7b6fb098d8770e4ffc9632
mahdi hosseini <hosseinim018@gmail.com>
2024-03-04 12:33:26 +0330

    config statics path

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: 'http://localhost:8080',
+  outputDir: '../static/dist',
+  indexPath: '../../templates/_base_vue.html',
+
+  configureWebpack: {
+    devServer: {
+      devMiddleware: {
+        writeToDisk: true
+      }
+    }
+  }
 })
