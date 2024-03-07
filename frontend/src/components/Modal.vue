611fbf4f6844846ff53ca438bcd218a7dac108af
mahdi hosseini <hosseinim018@gmail.com>
2024-03-07 21:31:32 +0330

    updated

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -11,7 +11,7 @@ export default {
 
 <template>
 <button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-Toggle modal
+  <slot name="button"></slot>
 </button>
 
 <div id="popup-modal" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
@@ -72,19 +72,19 @@ Toggle modal
                     </svg>
                     <span class="sr-only">Refresh icon</span>
                 </div>
-                <div class="ms-3 text-sm font-normal">
-                    <span class="mb-1 text-sm font-semibold text-gray-900 dark:text-white">Update available</span>
-                    <div class="mb-2 text-sm font-normal">A new software version is available for download.</div>
-                    <div class="grid grid-cols-2 gap-2">
-                        <div>
-                            <a href="#" class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:ring-blue-800">Update</a>
-                        </div>
-                        <div>
-                            <a href="#" class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Not now</a>
-                        </div>
+                <div class="ms-3 text-sm font-normal flex flex-col gap-2">
+                    <span class="mb-1 text-sm font-semibold text-gray-900 dark:text-white">
+                      <slot name="title"></slot>
+                    </span>
+                    <div class="mb-2 text-sm font-normal"><slot name="text"></slot></div>
+                    <div class="grid grid-cols-2 gap-2 items-center justify-center">
+                      <slot name="btns"></slot>
+                      <div>
+                        <button data-modal-hide="popup-modal" aria-label="Close" type="button" class="inline-flex justify-center w-full px-2 py-1.5 text-xs font-medium text-center text-gray-900 bg-white border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-600 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">لغو</button>
+                      </div>
                     </div>
                 </div>
-                <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white items-center justify-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-modal-hide="popup-modal" aria-label="Close">
+              <button type="button" class="ms-auto -mx-1.5 -my-1.5 bg-white items-center justify-center flex-shrink-0 text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700" data-modal-hide="popup-modal" aria-label="Close">
                     <span class="sr-only">Close</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -92,9 +92,6 @@ Toggle modal
                 </button>
             </div>
         </div>
-
     </div>
 </div>
-  <template v-if="type == 'error'"></template>
-  <template v-if="type == 'confirm'"></template>
 </template>
