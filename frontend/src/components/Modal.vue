886ab85f5c6366c11bd765a06513a4c451c01b68
mahdi hosseini <hosseinim018@gmail.com>
2024-03-10 23:38:27 +0330

    updated

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -10,7 +10,7 @@ export default {
 </script>
 
 <template>
-<button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+<button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="w-44 block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
   <slot name="button"></slot>
 </button>
 
