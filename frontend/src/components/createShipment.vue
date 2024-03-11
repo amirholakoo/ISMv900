f4300f5f9dea6b6949f5fee40f4c5b76fc1375c3
mahdi hosseini <hosseinim018@gmail.com>
2024-03-11 11:51:00 +0330

    updated

--- frontend/src/components/createShipment.vue
+++ frontend/src/components/createShipment.vue
@@ -9,16 +9,14 @@ export default {
   components: {Card, Alert, modal},
   data(){
     return {
-      drowpdownList: {
-        license_number: {name: 'شماره پلاک',title: 'شماره پلاک', data: '', value: ''},
-        supplier_name: {name: 'اسم تامین کننده',title: 'اسم تامین کننده', data: '', value: ''},
-        matrial_type: {name: 'نوع ماده',title: 'نوع ماده', data: '', value: ''},
-        matrial_name: {name: 'اسم ماده',title: 'اسم ماده', data: '', value: ''},
-        shipmet_type: {name: 'نوع بار نامه',title: 'نوع بار نامه', data: '', value: ''},
-        customer_name: {name: 'اسم مشتری',title: 'اسم مشتری', data: '', value: ''},
-      },
       forms: {
-        username: {name: 'نام کاربری', value: ''},
+        lic_number: {type: 'dropdown', name: 'شماره پلاک',title: 'شماره پلاک', data: '', value: ''},
+        supplier_name: {type: 'dropdown', name: 'اسم تامین کننده',title: 'اسم تامین کننده', data: '', value: ''},
+        matrial_type: {type: 'dropdown', name: 'نوع ماده',title: 'نوع ماده', data: '', value: ''},
+        matrial_name: {type: 'dropdown', name: 'اسم ماده',title: 'اسم ماده', data: '', value: ''},
+        shipmet_type: {type: 'dropdown', name: 'نوع بار نامه',title: 'نوع بار نامه', data: '', value: ''},
+        customer_name: {type: 'dropdown', name: 'اسم مشتری',title: 'اسم مشتری', data: '', value: ''},
+        username: {type: 'input', name: 'نام کاربری', value: ''},
       },
       success: false,
       error: false,
@@ -29,43 +27,43 @@ export default {
     initFlowbite();
     this.axios.get('/myapp/api/getLicenseNumbers').then((response) => {
       console.log(response.data)
-      this.drowpdownList.license_number.data = response.data['license_numbers']
+      this.drowpdownList.lic_number.data = response.data['license_numbers']
     })
     this.axios.get('/myapp/api/getAnbarTableNames').then((response) => {
       console.log(response.data)
-      this.drowpdownList.loading_location.data = response.data['data']
+      this.drowpdownList.unloading_location.data = response.data['data']
     })
     this.axios.get('/myapp/api/getUnitNames').then((response) => {
       console.log(response.data)
-      this.drowpdownList.width.data = response.data['unit_names']
+      this.drowpdownList.unit.data = response.data['unit_names']
     })
   },
   methods:{
     clicked(k, name){
       console.log(k, name)
-      if (k == 'license_number'){
-        this.drowpdownList.license_number.name = name
-        this.drowpdownList.license_number.value = name
+      if (k == 'lic_number'){
+        this.drowpdownList.lic_number.name = name
+        this.drowpdownList.lic_number.value = name
       }
-      if (k == 'loading_location'){
-        this.drowpdownList.loading_location.name = name
-        this.drowpdownList.loading_location.value = name
+      if (k == 'unloading_location'){
+        this.drowpdownList.unloading_location.name = name
+        this.drowpdownList.unloading_location.value = name
       }
-      if (k == 'width'){
-        this.drowpdownList.width.name = name
-        this.drowpdownList.width.value = name
+      if (k == 'unit'){
+        this.drowpdownList.unit.name = name
+        this.drowpdownList.unit.value = name
       }
     },
     async addSupplier() {
       const params = {
-        "license_number": this.drowpdownList.license_number.value,
-        "loading_location": this.drowpdownList.loading_location.value,
-        "width": this.drowpdownList.width.value,
+        "license_number": this.drowpdownList.lic_number.value,
+        "unloading_location": this.drowpdownList.unloading_location.value,
+        "unit": this.drowpdownList.unit.value,
         "quantity": this.forms.Quantity.value,
         "quality": this.forms.Quality.value,
         "forklift_driver": this.forms.forklift_driver.value
       };
-      const response = await this.axios.post('/myapp/api/loaded', {}, {params: params})
+      const response = await this.axios.post('/myapp/api/unload', {}, {params: params})
       console.log(response.data); // Access response data
       if (response.data['status'] == 'success'){
         this.success = true
@@ -79,7 +77,7 @@ export default {
 </script>
 
 <template>
- <Card title="افزودن بارنامه">
+   <Card title="افزودن بارنامه">
     <div class="flex flex-col gap-2 justify-center items-center">
       <div v-if="error" class="flex p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
             <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
@@ -93,37 +91,38 @@ export default {
               </ul>
             </div>
           </div>
-      <div class="relative" v-for="(val, form_name) in forms">
-        <input v-model="val.value" type="text" :id="form_name" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
-        <label :for="form_name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
-          {{val.name}}
-        </label>
-      </div>
-      <template v-for="(val, dropdown) in drowpdownList">
-        <button :id="dropdown + 'Button'" :data-dropdown-toggle="dropdown+'dropdown'" class="justify-between w-44 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-          {{val.name}}
-          <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
-          </svg>
-        </button>
-        <!-- Dropdown menu -->
-        <div :id="dropdown+'dropdown'" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-            <ul class="overflow-y-auto h-48 py-2 text-sm text-gray-700 dark:text-gray-200" :aria-labelledby="dropdown + 'Button'">
+      <template v-for="(val, form_name) in forms">
+        <template v-if="val.type=='input'">
+          <div class="relative">
+            <input v-model="val.value" type="text" :id="form_name" class="block px-2.5 pb-2.5 pt-4 w-full text-sm text-gray-900 bg-transparent rounded-lg border-1 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder="" />
+            <label :for="form_name" class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1">
+              {{val.name}}
+            </label>
+          </div>
+        </template>
+        <template v-else>
+          <button :id="form_name + 'Button'" :data-dropdown-toggle="form_name+'dropdown'" class="justify-between w-44 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+            {{val.name}}
+            <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+            </svg>
+          </button>
+          <!-- Dropdown menu -->
+          <div :id="form_name+'dropdown'" class="z-50 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
+            <ul class="overflow-y-auto h-48 py-2 text-sm text-gray-700 dark:text-gray-200" :aria-labelledby="form_name + 'Button'">
               <li v-for="data in val.data">
-                <a @click='clicked(dropdown ,data)' type="button" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
+                <a @click='clicked(form_name ,data)' type="button" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">
                   {{ data }}
                 </a>
               </li>
             </ul>
         </div>
+        </template>
       </template>
       <modal type="confirm">
         <template v-slot:button>اضافه کردن</template>
         <template v-slot:text>
           <div class="flex flex-col gap-2 font-bold">
-            <template v-for="(val, key) in drowpdownList">
-              <p>{{val.title}}: {{val.value}}</p>
-            </template>
             <template v-for="(val, key) in forms">
                 <p>{{val.name}}: {{val.value}}</p>
             </template>
@@ -135,11 +134,11 @@ export default {
           </div>
         </template>
       </modal>
-      <!--  <Alert v-if="success" type="Success">-->
-      <!--    <template v-slot:SuccessContent>-->
-      <!--      ماده حام  {{material_name}} - {{ material_type }} با موفقیت به دیتابیس اضافه شد.-->
-      <!--    </template>-->
-      <!--  </Alert>-->
+    <!--  <Alert v-if="success" type="Success">-->
+    <!--    <template v-slot:SuccessContent>-->
+    <!--      ماده حام  {{material_name}} - {{ material_type }} با موفقیت به دیتابیس اضافه شد.-->
+    <!--    </template>-->
+    <!--  </Alert>-->
     </div>
-  </Card>
+   </Card>
 </template>
