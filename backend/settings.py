35fd938461d6fbd26eab05a8ebd3617c5937f9bf
mahdi hosseini <hosseinim018@gmail.com>
2024-03-15 12:36:58 +0330

    config time to iran/tehran and changed language to farsi

--- backend/settings.py
+++ backend/settings.py
@@ -109,14 +109,26 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+# LANGUAGE_CODE = "en-us"
+#
+# TIME_ZONE = "UTC"
+#
+# USE_I18N = True
+#
+# USE_TZ = True
 
-TIME_ZONE = "UTC"
+# settings.py
+
+LANGUAGE_CODE = 'fa' # Farsi
+
+TIME_ZONE = 'Asia/Tehran'
+DEFAULT_CHARSET = 'utf-8'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_L10N = True
 
+USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
