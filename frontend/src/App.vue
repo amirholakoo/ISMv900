38184cc5f29e767dca8ea9fd1292018452e409b1
mahdi hosseini <hosseinim018@gmail.com>
2024-03-16 21:59:43 +0330

    removed footer and navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,45 +1,7 @@
 <template>
 <div class="flex flex-col container mx-auto gap-6 max-w-2xl">
-
-<!--  <main class="flex-grow flex flex-col justify-center">-->
-<!--    <router-view/>-->
-<!--  </main>-->
   <main class="flex flex-col justify-center items-center p-5">
-   <router-view/>
+    <router-view/>
   </main>
-
 </div>
-
-<!--  background-->
-<!--<div class="flex flex-col items-center justify-center max-w-2xl h-screen mx-auto m-6">-->
-<!--  <router-view/>-->
-<!--</div>-->
-
-<!--<button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">-->
-<!--  Dropdown button-->
-<!--  <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">-->
-<!--    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>-->
-<!--  </svg>-->
-<!--</button>-->
-
-<!--&lt;!&ndash; Dropdown menu &ndash;&gt;-->
-<!--<div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">-->
-<!--    <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">-->
-<!--      <li>-->
-<!--        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>-->
-<!--      </li>-->
-<!--      <li>-->
-<!--        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>-->
-<!--      </li>-->
-<!--      <li>-->
-<!--        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>-->
-<!--      </li>-->
-<!--      <li>-->
-<!--        <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>-->
-<!--      </li>-->
-<!--    </ul>-->
-<!--</div>-->
-
-
-
 </template>
