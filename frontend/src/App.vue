285fe00efc8eea10fdac129c558993dd7ba15e71
mahdi hosseini <hosseinim018@gmail.com>
2024-03-16 21:46:46 +0330

    removed footer and navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,59 +1,13 @@
 <template>
 <div class="flex flex-col container mx-auto gap-6 max-w-2xl">
-  <nav class="flex-none bg-white rounded-lg shadow mt-2 border-gray-200 dark:bg-gray-900">
-    <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-          <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">ISM</span>
-      </a>
-      <button data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-          <span class="sr-only">Open main menu</span>
-          <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-          </svg>
-      </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
-        <ul class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-          <li>
-            <a href="#" class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Home</a>
-          </li>
-          <li>
-            <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-          </li>
-          <li>
-            <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</a>
-          </li>
-          <li>
-            <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</a>
-          </li>
-          <li>
-            <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <main class="flex-grow flex flex-col justify-center">
-    <router-view/>
+
+<!--  <main class="flex-grow flex flex-col justify-center">-->
+<!--    <router-view/>-->
+<!--  </main>-->
+  <main class="flex flex-col justify-center items-center p-5">
+   <router-view/>
   </main>
-  <footer class="flex-none bg-white rounded-lg shadow mb-2 dark:bg-gray-800">
-   <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-     <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024 <a class="hover:underline">ISM™</a>. All Rights Reserved.</span>
-     <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
-       <li>
-         <a href="#" class="hover:underline me-4 md:me-6">About</a>
-       </li>
-       <li>
-         <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
-       </li>
-       <li>
-         <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
-       </li>
-       <li>
-         <a href="#" class="hover:underline">Contact</a>
-       </li>
-     </ul>
-   </div>
-  </footer>
+
 </div>
 
 <!--  background-->
