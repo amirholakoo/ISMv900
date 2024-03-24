206e72da3ba25f5a31a9e0eca2f70343ca5d8cb8
amirholakoo <amirholakoo@gmail.com>
2024-03-24 07:57:49 -0700

    Update add2urls.py

--- auto/add2urls.py
+++ auto/add2urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('fetch_sales_report/', views.fetch_sales_report, name='fetch_sales_report'),
     path('fetch_ongoing_shipments/', views.fetch_ongoing_shipments, name='fetch_ongoing_shipments'),
+    path('fetch_consumption_report/', views.fetch_consumption_report, name='fetch_consumption_report'),
 ]
