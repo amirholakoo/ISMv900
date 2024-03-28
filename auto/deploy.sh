59bd58227dfc69bfadf9c924bb0b8c9b6a39d875
amirholakoo <amirholakoo@gmail.com>
2024-03-28 19:02:46 +0330

    Add files via upload

--- auto/deploy.sh
+++ auto/deploy.sh
@@ -20,21 +20,26 @@ cd ~/V9/v988/
 echo "Rename old project folder with a timestamp"
 mv ISMv900 "ISMv900_old_$(date +%Y%m%d%H%M%S)"
 
-#echo " Remove old virtual environment"
-#rm -rf venv
-
 echo " Clone new version of the project"
 git clone https://github.com/amirholakoo/ISMv900.git
+
+#echo " Remove old virtual environment"
+#rm -rf venv
+#
+
 
 echo " Change ownership of the new project folder and virtual environment (assuming they're already created at some point)"
 sudo chown -R $USER:$USER ISMv900
 
+echo " Navigate to the ISM directory"
+cd ISMv900
+
 echo " Install Python3, pip, and virtualenv if not already installed"
 sudo apt-get install -y python3 python3-pip
-# sudo pip3 install virtualenv
+sudo pip3 install virtualenv
 
 echo " Create a new virtual environment and activate it"
-# python3 -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 
 echo " Install Django and jdatetime"
@@ -42,15 +47,16 @@ pip install django
 pip install jdatetime
 pip install pandas
 pip install openpyxl
+pip install qrcode
 
 # OR install all requirements from a file
-# pip install -r requirements.txt
+#pip install -r requirements.txt
 
 echo " Create static/dist directories in the project root"
-mkdir -p ISMv900/static/dist
+mkdir -p static/dist
 
 echo " Navigate to the frontend directory"
-cd ISMv900/frontend
+cd frontend
 
 echo " Install npm and node packages"
 sudo apt install -y npm
@@ -71,5 +77,5 @@ python manage.py createsuperuser
 # Note: Automated superuser creation is complex and might require a separate script or manual process
 
 echo " Starting servers "
-# python manage.py runserver &
-# cd frontend && npm run serve &
+python manage.py runserver &
+cd frontend && npm run serve &
