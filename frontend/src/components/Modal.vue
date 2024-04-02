660b8dd220e5b685758c54c28da6244c98c1b9a3
mahdi hosseini <hosseinim018@gmail.com>
2024-04-02 18:50:55 +0330

    add prop disabled

--- frontend/src/components/Modal.vue
+++ frontend/src/components/Modal.vue
@@ -2,7 +2,18 @@
 import { initFlowbite } from 'flowbite'
 export default {
   name: "Modal",
-  props: ['type'],
+  // props: ['type', 'disabled'],
+  props: {
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: true
+    },
+  },
   mounted() {
     initFlowbite();
   },
@@ -10,7 +21,12 @@ export default {
 </script>
 
 <template>
-<button data-modal-target="popup-modal" data-modal-toggle="popup-modal" class="md:w-44 sm:w-auto block text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+<button
+    :disabled="disabled"
+    data-modal-target="popup-modal"
+    data-modal-toggle="popup-modal"
+    class="md:w-44 sm:w-auto block text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+    type="button">
   <slot name="button"></slot>
 </button>
 
