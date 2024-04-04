a5c959573a52ad5835e06248b8a63cde3dd13063
mahdi hosseini <hosseinim018@gmail.com>
2024-04-04 16:07:33 +0330

    remove button tag and use class to route-link

--- frontend/src/components/admin/AllPages.vue
+++ frontend/src/components/admin/AllPages.vue
@@ -108,9 +108,13 @@ export default {
   <Card title="All Pages">
     <div class="flex flex-col gap-2 justify-center items-center">
       <template v-for="route in routes" :key="route.name">
-        <button type="button" class="w-44 block text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-          <router-link :to="route.path">{{ route.name }}</router-link>
-        </button>
+          <router-link
+              :to="route.path"
+              type="button"
+              class="w-44 block text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          >
+            {{ route.name }}
+          </router-link>
       </template>
     </div>
   </Card>
