5b0cefee8e209fab3647a82d9be109d39a2c01fe
mahdi hosseini <hosseinim018@gmail.com>
2024-05-10 03:13:07 +0330

    update

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'http://localhost:8080',
-  outputDir: '../static/dist',
+  publicPath: process.env.NODE_ENV === 'production' ? '/static/' : 'http://localhost:8080',
+  outputDir: '../static/',
   indexPath: '../templates/_base_vue.html',
 
   configureWebpack: {
     devServer: {
       devMiddleware: {
-        writeToDisk: true
+        writeToDisk: true // Only necessary if you need Django to serve these files in development
       }
     }
   }
