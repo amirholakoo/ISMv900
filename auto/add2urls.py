cc4896307f4f70999b7db817a090f671886f6d8c
Amir <amirholakoo@gmail.com>
2024-05-10 15:20:10 +0330

    Cleanups
    Design pdfs are zipped.
    Automation older versions were zipped and cleaned.

--- auto/add2urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('fetch_sales_report/', views.fetch_sales_report, name='fetch_sales_report'),
-    path('fetch_ongoing_shipments/', views.fetch_ongoing_shipments, name='fetch_ongoing_shipments'),
-    path('fetch_consumption_report/', views.fetch_consumption_report, name='fetch_consumption_report'),
-]
