319224a51c88aa0e9578dc13acea90dbe80ae87c
mahdi hosseini <hosseinim018@gmail.com>
2024-05-10 14:27:39 +0330

    config: Update ALLOWED_HOSTS and STATIC_URL for production environment

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-j%!2v7#94&n-a#&lvafcq3x!u@^2$-gb5c4i%65hkst%bf-@g2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.232.13']
 
 
 # Application definition
@@ -138,7 +138,7 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
