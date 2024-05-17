4561c3eb1f5c78a01d0179bb32c5f0c64caa0f29
mahdi hosseini <hosseinim018@gmail.com>
2024-05-17 23:26:12 +0330

    feat: handle missid v-if="success" condition in line 297

--- frontend/src/components/addTruck.vue
+++ frontend/src/components/addTruck.vue
@@ -294,7 +294,7 @@ export default {
             </ul>
           </div>
         </div>
-    <div class="flex p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+    <div v-if="success" class="flex p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
       <div class="flex flex-col items-center gap-2 sm:flex-row sm:items-center sm:gap-1 sm:space-x-4 mt-2">
         <svg class="flex-shrink-0 inline w-4 h-4 me-3 mt-[2px]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
           <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
