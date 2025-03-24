3e31fe4edc6874cec6fe15dee2abe6b2490c2a0c
Amir <amirholakoo@gmail.com>
2025-03-24 18:45:06 +0330

    Switch back to sqlite3

--- backend/settings.py
+++ backend/settings.py
@@ -77,22 +77,22 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+ DATABASES = {
+     "default": {
+         "ENGINE": "django.db.backends.sqlite3",
+         "NAME": BASE_DIR / "db.sqlite3",
+     }
+ }
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ISM',
-        'USER': 'postgres',
-        'PASSWORD': '62443444',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'ISM',
+#        'USER': 'postgres',
+#        'PASSWORD': '62443444',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#    }
+#}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
